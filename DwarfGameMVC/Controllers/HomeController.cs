253335dce7628959ee9c1ec4500f5ae63058298d
fernandvos <fernandvos@yahoo.co.uk>
2018-08-15 09:45:50 +0200

    First commit

---
+++ DwarfGameMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DwarfGameMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Rules()
+        {
+            ViewBag.Message = "The rules to the game.";
+
+            return View();
+        }
+    }
+}
